48c7cef773d2ab7ef9533ff8a5b8571557e5690a
Gokul Kannan <gokulkannan@Gokuls-MacBook-Air.local>
2018-06-28 20:36:52 +0530

    Initial commit

---
+++ app/src/main/java/happy/fox/climatography/tasks/TaskOutput.java
@@ -0,0 +1,8 @@
+package happy.fox.climatography.tasks;
+
+public class TaskOutput {
+    // Indicates result of parsing server response
+    ParseResult parseResult;
+    // Indicates result of background task
+    TaskResult taskResult;
+}
---
+++ app/src/main/java/happy/fox/climatography/tasks/TaskResult.java
@@ -0,0 +1,3 @@
+package happy.fox.climatography.tasks;
+
+public enum TaskResult { SUCCESS, BAD_RESPONSE, IO_EXCEPTION, TOO_MANY_REQUESTS; }
